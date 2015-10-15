7d6a76ba7a5f45299802434071390d069c07d861
Dillon Drobena <dillon.drobena@utdallas.edu>
2015-10-15 18:34:23 -0500

    Added editing capabilities for users, as well as admin privileges

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,6 +4,7 @@ class UsersControllerTest < ActionController::TestCase
 
   def setup
     #Need default user
+    @user2 = users(:toppy)
     @user = User.new(name: "Test User", email: "test.user@gmail.com", password: "foobar", password_confirmation: "foobar")
     @user.save
   end
@@ -24,6 +25,32 @@ class UsersControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
     assert_response :success
+  end
+
+  test "should redirect edit when not logged in" do
+    get :edit, id: @user
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@user2)
+    get :edit, id: @user
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@user2)
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    assert flash.empty?
+    assert_redirected_to root_url
   end
 
 end
