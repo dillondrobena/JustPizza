642d993782686acc90126f268b869a2b37db95e8
Dillon Drobena <dillon.drobena@utdallas.edu>
2015-10-21 17:29:46 -0500

    Added pizza test

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,4 +1,5 @@
 class Pizza < ActiveRecord::Base
   has_and_belongs_to_many :toppings
   belongs_to :crust
+  validates :crust, presence: true
 end
--- test/models/pizza_test.rb
+++ test/models/pizza_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class PizzaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @pizza = Pizza.create(name: "Sample Pizza")
+  end
+  test "crust should be present" do
+    assert_not @pizza.valid?, "Crust should not be empty"
+  end
 end
