81629e56028ad5eff751f49a2f39c51eb3c79d14
Shermm <helioblast86@gmail.com>
2015-11-25 09:21:21 -0600

    added pizza_path attribute to pizza db table for storing path to picture, also added cart button to page header

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151030220032) do
+ActiveRecord::Schema.define(version: 20151118194046) do
 
   create_table "crusts", force: :cascade do |t|
     t.string   "name"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20151030220032) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "pizza_id"
+    t.string   "pizza_path"
   end
 
   create_table "toppings", force: :cascade do |t|
